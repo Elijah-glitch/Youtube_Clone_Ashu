089395be531404ba52a5380c94554a7a192b60ed
Elijah-glitch <62041760+Elijah-glitch@users.noreply.github.com>
2022-12-15 10:14:58 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA74ZvfzfZVmopLVsbl88Y02CUaXNWGgwk,
-    authDomain: "yt-clone-ashu.firebaseapp.com",
-    projectId: "yt-clone-ashu",
-    storageBucket: "yt-clone-ashu.appspot.com",
-    messagingSenderId: "178146130104",
-    appId: "1:178146130104:web:cec6098e0f25e3d6db5c4d"
+  apiKey: "AIzaSyA74ZvfzfZVmopLVsbl88Y02CUaXNWGgwk",
+  authDomain: "roblox-29707.firebaseapp.com",
+  projectId: "roblox-29707",
+  storageBucket: "roblox-29707.appspot.com",
+  messagingSenderId: "1025034692104",
+  appId: "1:1025034692104:web:628690bd4a9e9bf83293bd",
+};
+
   };
 
   firebase.initializeApp(firebaseConfig);
