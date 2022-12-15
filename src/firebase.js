bbba89290f52485b71e88c2c94ee35d8c97bfe2b
Elijah-glitch <62041760+Elijah-glitch@users.noreply.github.com>
2022-12-15 10:12:38 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDl20gKEOq0Yw-EFvd0AHbWKXN1nvcotTs",
+    apiKey: "AIzaSyA74ZvfzfZVmopLVsbl88Y02CUaXNWGgwk,
     authDomain: "yt-clone-ashu.firebaseapp.com",
     projectId: "yt-clone-ashu",
     storageBucket: "yt-clone-ashu.appspot.com",
